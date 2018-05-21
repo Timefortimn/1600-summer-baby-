e6fcb494851b797e298f2ffc3cbb85636ed1bbca
Unknown <tjhamson@gmail.com>
2018-05-21 11:20:17 -0600

    Created Animals

--- Dope Scripting 1600/Assets/Scripts/AnimalBehavior.cs
+++ Dope Scripting 1600/Assets/Scripts/AnimalBehavior.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spacessuckstartandupdate : MonoBehaviour {
+public class AnimalBehavior : MonoBehaviour
+{
+
+    public Animal AnimalInfo;
 
 	// Use this for initialization
 	void Start () {
-		
+        print(AnimalInfo.Health);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        print("Hello World");
+		
 	}
 }
