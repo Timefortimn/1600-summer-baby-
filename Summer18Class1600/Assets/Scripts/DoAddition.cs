6af60820e40c2108e66ba7eb99c8ea735354f774
Unknown <tjhamson@gmail.com>
2018-06-06 11:43:05 -0600

    New Project with color change behaviours

---
+++ Summer18Class1600/Assets/Scripts/DoAddition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DoAddition : ScriptableObject
+{
+    private int c;
+
+    private void OnEnable()
+    {
+        c = Run(20, 2);
+    }
+    public int Run(int a, int b)
+    {
+        return a += b;
+    }
+}
---
+++ Summer18Class1600/Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour
+{
+
+    public FloatData HealthLevel;
+    public FloatData EvilPower;
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+        HealthLevel.Value -= EvilPower.Value;
+    }
+}
