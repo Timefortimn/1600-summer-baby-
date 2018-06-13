e59c1efc81845ad9e549ca9258b3eeb16c3c2878
Unknown <tjhamson@gmail.com>
2018-06-13 01:34:45 -0600

    Camera follow and walls

--- Summer18Class1600/Assets/Scripts/MovePlayer.cs
+++ Summer18Class1600/Assets/Scripts/MovePlayer.cs
@@ -1,31 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
     private CharacterController controller;
-    private Vector3 newPosition;
+	private Vector3 newPosition;
 
+	public bool IsGrounded;
     public float Speed = 10.0f;
-    public float Gravity = 6.0f;
+    public float Gravity = 4.0f;
     public float JumpSpeed = 50.0f;
 
 	void Start ()
     {
         controller = GetComponent<CharacterController>();
+    }
+
+	void OnCollisionStay()
+	{
+		IsGrounded = true;
 	}
 	
 	void Update ()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
+	{
+		
+		float mouseXInput = Input.GetAxis("Mouse X");
+		Vector3 lookhere = new Vector3(0,mouseXInput,0);
+		transform.Rotate(lookhere);
+	    
+	    if (Input.GetKeyDown(KeyCode.Space)&& controller.isGrounded)
         {
             newPosition.y = JumpSpeed;
-        }
+	        IsGrounded = false;
 
-        newPosition.x = Speed * Input.GetAxis("Horizontal");
-        newPosition.z = Speed * Input.GetAxis("Vertical");
-        newPosition.y -= Gravity;
+        }
+		newPosition.y -= Gravity;
         controller.Move(newPosition * Time.deltaTime);
 	}
 }
