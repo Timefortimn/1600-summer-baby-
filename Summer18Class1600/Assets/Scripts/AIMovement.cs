eb7a17256f4b6ec8b7a1cccf950e46a671dd6f54
Unknown <tjhamson@gmail.com>
2018-07-09 12:28:20 -0600

    AI

---
+++ Summer18Class1600/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AIMovement : MonoBehaviour
+{
+	private NavMeshAgent Agent;
+	public Transform Destination;
+
+	private void Start ()
+	{
+		Agent = GetComponent<NavMeshAgent>();
+	}
+	
+	private void Update () 
+	{
+		Agent.destination = Destination.position;
+		
+	}
+}
---
+++ Summer18Class1600/Assets/Scripts/MoveEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class MoveEnemy : NetworkBehaviour
+{
+	private NavMeshAgent Agent;
+	public Transform TargetPlayer;
+	
+	public override void OnStartServer()
+	{
+		Agent = GetComponent<NavMeshAgent>();
+	}
+
+	void Update () {
+		
+	}
+}
