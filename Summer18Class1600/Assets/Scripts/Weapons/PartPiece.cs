d23e9553167cc1bdd9c29327eea8b566a96c232e
Unknown <tjhamson@gmail.com>
2018-07-16 11:42:33 -0600

    Ball Bounce
    Also practicing collections

---
+++ Summer18Class1600/Assets/Scripts/Weapons/PartPiece.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartPiece : MonoBehaviour {
+
+	public float DamageLevel = 20.0f;
+
+	public float DoDamage()
+	{
+		return DamageLevel;
+	}
+}
---
+++ Summer18Class1600/Assets/Scripts/Weapons/Parts.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts : MonoBehaviour
+{
+
+	public enum PartType
+	{
+		Fmj,
+		Clip,
+		Scope,
+		Silencer,
+		SemiAuto
+	}
+
+	public PartPiece[] PartPieces;
+
+	public PartType CurrentPart;
+
+	void Update()
+	{
+		switch (CurrentPart)
+		{
+			case PartType.Fmj:
+				Debug.Log(PartPieces[0].name);
+				Debug.Log(PartPieces[0].DoDamage());
+				break;
+			case PartType.Clip:
+				Debug.Log(PartPieces[1].name);
+				Debug.Log(PartPieces[1].DoDamage());
+				break;
+			case PartType.Scope:
+				Debug.Log(PartPieces[2].name);
+				Debug.Log(PartPieces[2].DoDamage());
+				break;
+			case PartType.Silencer:
+				Debug.Log(PartPieces[3].name);
+				Debug.Log(PartPieces[3].DoDamage());
+				break;
+			case PartType.SemiAuto:
+				Debug.Log(PartPieces[4].name);
+				Debug.Log(PartPieces[4].DoDamage());
+				break;
+
+		}
+	}
+}
