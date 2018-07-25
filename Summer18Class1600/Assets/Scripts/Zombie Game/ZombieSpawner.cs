9cd5c44d43b44bfcb8d7fdf2658a5d5d585b87c2
Unknown <tjhamson@gmail.com>
2018-07-25 12:07:23 -0600

    Game States and Zombie AI

---
+++ Summer18Class1600/Assets/Scripts/Zombie Game/ZombieSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawner : MonoBehaviour {
+
+	public GameObject ZombiePrefab;
+	public GameObject SpawnLocation;
+	public Transform TargetPlayer;
+	public int numberOfEnemies;
+
+	public void Start()
+	{
+		StartCoroutine(SpawnEnemy());
+	}
+
+	private IEnumerator SpawnEnemy()
+	{
+		while (numberOfEnemies > 0)
+		{
+			for (int i = 0; i < numberOfEnemies; i++)
+			{
+				var spawnPosition = SpawnLocation.transform.position +
+				                    new Vector3(Random.Range(-8.0f, 8.0f), 0.0f, Random.Range(-8.0f, 8.0f));
+				var spawnRotation = Quaternion.Euler(0.0f, Random.Range(0, 180), 0.0f);
+				Instantiate(ZombiePrefab, spawnPosition, spawnRotation);
+			}
+
+			numberOfEnemies--;
+            
+			yield return new WaitForSeconds(5);
+		}
+        
+	}
+}
