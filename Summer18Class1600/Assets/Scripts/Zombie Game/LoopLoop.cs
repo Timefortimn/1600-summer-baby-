a06ad10d1da5d13fe1eed02d2f764a2a165b7f06
Unknown <tjhamson@gmail.com>
2018-08-08 23:54:41 -0600

    A little last handy work

--- Summer18Class1600/Assets/Scripts/Zombie Game/LoopLoop.cs
+++ Summer18Class1600/Assets/Scripts/Zombie Game/LoopLoop.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoopLoop : MonoBehaviour {
+public class LoopLoop : MonoBehaviour
+{
+    int numEnemies = 3;
+    int Zombies = 4;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Start()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+
+        while (Zombies > 0)
+        {
+            Debug.Log("Fight! Get 'em!");
+            Zombies--;
+        }
+        string[] strings = new string[3];
+        
+        strings[0] = "You're";
+        strings[1] = "Doing";
+        strings[2] = "So Great!";
+        
+        foreach(string item in strings)
+        {
+            print (item);
+        }
+    }
 }
